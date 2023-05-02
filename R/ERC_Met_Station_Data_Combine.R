b0daf6c7d176d6f503e85986b32663d93d427b3a
Erin Graham <erin.graham@jcu.edu.au>
2023-05-02 13:46:58 +1000

    remove daily code; add more debug code; check warnings

--- R/ERC_Met_Station_Data_Combine.R
+++ R/ERC_Met_Station_Data_Combine.R
@@ -38,13 +38,16 @@ wrk$datetime <- as.POSIXct(strptime(wrk$timestamp, format="%d-%b-%y %I:%M:%S %p"
 
 # check output
 str(wrk)
-
+print('after datetime conversion...')
 
 #convert anything that should be numeric
-wrk$Air.Temp <- as.numeric(paste(wrk$Air.Temp))
+#wrk$Air.Temp <- as.numeric(paste(wrk$Air.Temp)) # already numeric
 
 #remove extraneous data from before the system was stable
 wrk<-subset(wrk,datetime>=as.POSIXct("2021-07-20 00:00:00"))
+
+print('after subset...')
+str(wrk)
 
 #remove nonsense data
 wrk$Solar.Radiation[wrk$Solar.Radiation >= 1500] <- NA
@@ -53,6 +56,9 @@ wrk$Air.Temp[wrk$Air.Temp >= 60|wrk$Air.Temp <= 10] <- NA
 #summarize to 5min data
 names(wrk)[names(wrk) == 'datetime'] <- 'date'
 
+print('after summarize to 5min...')
+str(wrk)
+
 #workout rainfall separately
 rain <- subset(wrk, select=c(date,Rainfall))
 wrk <- subset(wrk, select=-c(timestamp,Rainfall,Rainfall.Since.9am))
@@ -60,26 +66,24 @@ wrk <- subset(wrk, select=-c(timestamp,Rainfall,Rainfall.Since.9am))
 wrk_15min <- timeAverage(wrk, avg.time = "15 min", statistic = "mean")
 rain_sum <- timeAverage(rain, avg.time = "15 min", statistic = "sum")
 
+# after timeAverage
+str(wrk_15min)
+str(rain_sum)
+
 wrk_15min <- merge(wrk_15min, rain_sum)
 
-#Summarize daily data sets 
+#Summarize daily data sets # emg removed daily section of code
+# add column of just date (not time)
 wrk_15min$day <- as.POSIXct(
                 strftime(wrk_15min$date, format = "%Y-%m-%d %H:%M:%S"),
                 format = "%Y-%m-%d")
 
 # view output
+print('after smmarize daily...')
 str(wrk_15min)
-
-Daily <- ddply(wrk_15min, .(day), summarise,
-               Max_Temp=max(Air.Temp,na.rm=TRUE),
-               Av_Temp=mean(Air.Temp,na.rm=TRUE),
-               Min_Temp=min(Air.Temp,na.rm=TRUE),
-               Radiant_exposure=sum(Solar.Radiation*15*60,na.rm=TRUE)/1000,
-               Rainfall =sum(Rainfall,na.rm=TRUE))
-
-Daily[sapply(Daily, is.infinite)] <- NA
 
 ts_filename = paste0('ERC_Data_output/Current_cleaned_', 
                      substr(now(), 0, 10), '.csv')
 write.csv(wrk_15min, file=ts_filename, row.names=FALSE)
   
+warnings()
